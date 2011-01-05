ac7e5cdf5775d34eea91a3ddd82a7d7984fe3f64
Martin Brochhhaus <mbrochh@googlemail.com>
2011-01-06 00:18:29 +0100

    Fixed buggy ADMIN_MEDIA_PREFIX

--- webapps/django/project/settings.py
+++ webapps/django/project/settings.py
@@ -14,7 +14,7 @@ MEDIA_URL = '/static/'
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/media/'
+ADMIN_MEDIA_PREFIX = '/static/media/'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
