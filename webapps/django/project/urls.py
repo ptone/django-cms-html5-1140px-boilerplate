6d9643427a26a8e73b5d32b050c87dfc5b2c1e8a
Martin Brochhaus <mbrochh@gmail.com>
2011-06-29 14:10:09 +0800

    added serving of media and static files to urls.py

--- webapps/django/project/urls.py
+++ webapps/django/project/urls.py
@@ -1,19 +1,16 @@
 from django.conf import settings
-from django.conf.urls.defaults import *
+from django.conf.urls.defaults import patterns, url
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 # The next two lines enable the admin and load each admin.py file:
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns(
+urlpatterns = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += patterns(
     '',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('cms.urls')),
 )
-
-if settings.SANDBOX:
-    urlpatterns += patterns(
-        '',
-        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {
-            'document_root': settings.MEDIA_ROOT}),
-    )
