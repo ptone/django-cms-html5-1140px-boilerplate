26ecb75adf44fb02297472e55696ce7ff375dac8
ptone <preston@ptone.com>
2011-11-11 16:19:17 -0800

    updated to 1.4 layout

--- webapps/django/project/settings.py
+++ webapps/django/project/settings.py
@@ -7,7 +7,7 @@ from local_settings import *
 gettext = lambda s: s
 PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
 
-
+WSGI_APPLICATION = 'project.wsgi.application
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
 STATIC_ROOT = os.path.join(PROJECT_PATH, '..', '..', STATIC_APP_NAME)
@@ -141,3 +141,34 @@ INSTALLED_APPS = (
     'south',
     # your own apps
 )
+
+
+
+# A sample logging configuration. The only tangible logging
+# performed by this configuration is to send an email to
+# the site admins on every HTTP 500 error when DEBUG=False.
+# See http://docs.djangoproject.com/en/dev/topics/logging for
+# more details on how to customize your logging configuration.
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
